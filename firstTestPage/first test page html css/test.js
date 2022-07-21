5fa4659951545440751f90a33134eae74796def5
Abdullah MAHZUDDIN <abdullah.mahzuddin@Digital-Grenoble.local>
2022-07-21 10:10:49 +0200

    my first page html and css

---
+++ firstTestPage/first test page html css/test.js
@@ -0,0 +1,15 @@
+var formtx = document.querySelector('.gt-in-tch-form');
+formtx.style.color = 'yellow';
+
+var frm = document.querySelectorAll('li:nth-child (2)');
+for(var i=0; i<frm.length; i++){
+    frm[i].style.color = 'yellow'
+}
+
+var frm2 = document.querySelectorAll('#section');
+for(var i=0; i<frm2.length; i++){
+    frm2[i].style.color = 'skyblue'
+}
+
+var formbg = document.querySelector('.contact-form');
+formbg.style.color = 'white';
